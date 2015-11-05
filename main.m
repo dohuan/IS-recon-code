9acef3d73087a23ae1a5e8c9e0903baaef3187b8
dohuan <dohuan@msu.edu>
2015-11-05 08:49:26 -0500

    110515

--- main.m
+++ main.m
@@ -23,7 +23,7 @@ option.x_min = min_(1);
 option.y_min = min_(2);
 option.z_min = min_(3);
 
-gridsize = 50;
+gridsize = 100;
 option.x_mesh = linspace(option.x_min,option.x_max,gridsize);
 option.y_mesh = linspace(option.y_min,option.y_max,gridsize);
 option.z_mesh = linspace(option.z_min,option.z_max,gridsize);
@@ -42,7 +42,8 @@ hyp.lik = log(0.03);
 
 % --- Create random index of the cloud
 index = randperm(n);
-dp = [0.1 0.5 0.7 1]; % data portion
+%dp = [0.1 0.5 0.7 1]; % data portion
+dp = 1;
 for i=1:size(dp,2)
     ix = index(1:round(dp(i)*n));
     data(i).X = X(ix,:);
@@ -57,9 +58,11 @@ for i=1:size(dp,2)
         end
     end
     data(i).S_est = S_est;
+    plySave = ['./results/bunny_PLY_2/bunny_' num2str(size(data(i).X,1)) '.ply'];
+    exportMesh(S_est,plySave);
+    %figure(i)
+    %scatter3(S_est(:,1),S_est(:,2),S_est(:,3));
     
-    figure(i)
-    scatter3(S_est(:,1),S_est(:,2),S_est(:,3));
 end
 
 
